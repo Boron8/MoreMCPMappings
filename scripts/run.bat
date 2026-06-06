2a68a7322e8db034ae5e9b2b666b261a21d5e20c
Boron8 <218486241+Boron8@users.noreply.github.com>
2026-06-06 18:56:08 +0200

    Added convenience script for compiling and running the game

---
+++ scripts/run.bat
@@ -0,0 +1,7 @@
+@echo off
+
+runtime\bin\python\python_mcp runtime\recompile.py %*
+runtime\bin\python\python_mcp runtime\reobfuscate.py %*
+runtime\bin\python\python_mcp runtime\startclient.py %*
+
+pause
